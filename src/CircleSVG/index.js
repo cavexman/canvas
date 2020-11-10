328283051652cf84cebd0d40b3d69f49f9fc0920
Richard Cave <richard.cave@snapwi.re>
2020-11-09 16:23:20 -0800

    center text

--- src/CircleSVG/index.js
+++ src/CircleSVG/index.js
@@ -19,22 +19,27 @@ export const CircleSVG = ({
     setSize,
 }) => { // render prop
     const [ref, x, y, isDragging] = useDragging(() => setSelection([id]));
+    const size = Math.min(width, height); // circle wants width == height
     return (
         <div
             className={cls.frame}
             ref={ref}
             style={{
                 position: "absolute",
-                width, // TODO size of object so we can place handles
+                width,
                 height,
                 left: x,
                 top: y,
             }}
         >
-            <div className={cls.textblock}>{text}</div>
-            <svg width={width} height={height}>
-                <circle cx={width/2} cy={height/2} r={(width-1)/2} stroke={strokeColor} strokeWidth={strokeWidth} fill={fillColor} />
-            </svg>
+            <div className={cls.textFrame}>
+                <div className={cls.textblock}>{text}</div>
+            </div>
+            <div className={cls.frame}>
+                <svg width={width} height={height}>
+                    <circle cx={size/2} cy={size/2} r={(size)/2} stroke={strokeColor} strokeWidth={strokeWidth} fill={fillColor} />
+                </svg>
+            </div>
             {selected && <SelectionHandles width={width} height={height} setSize={setSize} />}
         </div>
     )
